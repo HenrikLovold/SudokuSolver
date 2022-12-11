3910660e0482928ca93ccbeb615eefeab3eb731f
Henrik Løvold <henrik.lovold@gmail.com>
2022-12-11 17:54:17 +0100

    Initial Commit

---
+++ SudokuSolver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  SudokuSolver
+//
+//  Created by Henrik Hillestad Løvold on 11/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
